e1bb21a97a8f5388c2b3e7662c5f22c0e30be0c3
unknown <brunjlar@WL306541.eu.tieto.com>
2015-05-27 08:39:52 +0200

    readObservable

--- src/Control/Monad/Knockout.purs
+++ src/Control/Monad/Knockout.purs
@@ -2,10 +2,12 @@ module Control.Monad.Knockout (
     KO (..),
     Observable (..),
     newObservable,
-    writeObservable
+    writeObservable,
+    readObservable
 ) where
 
 import Control.Monad.Eff
+import Data.Foreign
 
 foreign import data KO :: !
 
@@ -13,20 +15,30 @@ foreign import data Observable :: * -> *
 
 foreign import newObservable
     """
-    function newObservable () {
+    function newObservable() {
         return ko.observable();
-    };
+    }
     """
     :: forall a eff. Eff (ko :: KO | eff) (Observable a)
     
 foreign import writeObservable
     """
-    function writeObservable (obs) {
+    function writeObservable(obs) {
         return function (value) {
             return function () {
                 obs(value);
             };
         };
-    };
+    }
     """
     :: forall a eff. Observable a -> a -> Eff (ko :: KO | eff) Unit
+    
+foreign import readObservable
+    """
+    function readObservable(obs) {
+        return function () {
+            return obs();
+        };
+    }
+    """
+    :: forall a eff. Observable a -> Eff (ko :: KO | eff) Foreign
--- src/hello.purs
+++ src/hello.purs
@@ -2,10 +2,16 @@ module Hello where
 
 import Control.Monad.Eff
 import Control.Monad.Knockout
+import Data.Either
+import Data.Foreign
+import Data.Foreign.Class
 import Debug.Trace
 
 main = do
     obs <- newObservable
     writeObservable obs 42
-    trace "Hello, World!"
+    value <- readObservable obs
+    case (read value :: F Number) of
+        Left e  -> trace $ "error: " ++ show e
+        Right x -> trace $ "value: " ++ show x
     return { x: obs }
